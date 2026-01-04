92b6af0f452de03c7a3487afda699b99dfc21065
Siva <sivacs2004@gmail.com>
2026-01-04 06:07:59 +0000

    Initial stage

---
+++ scripts.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+touch test1.txt
+touch test2.txt
+echo "This is my first scripts files" > test1.txt
+echo "This is my second files" > test2.txt
+echo "This is the new files" >> test1.txt
+
