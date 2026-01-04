0b128d52f577c98de5d142e4e59708804492bcc5
Siva <sivacs2004@gmail.com>
2026-01-04 06:38:49 +0000

    Initial stage

--- scripts.sh
+++ scripts.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+
+for i in {1..10}
+do
+  if [ $i -eq 5 ]; then
+    echo "Breaking at $i"
+	break
+  fi 
+  echo "Number: $i"
+done 
 touch test1.txt
 touch test2.txt
 echo "This is my first scripts files" > test1.txt
